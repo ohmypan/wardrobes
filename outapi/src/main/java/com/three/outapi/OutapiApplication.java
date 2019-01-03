6fac1fe80eef9c57c6959bcd92d19c51e93dd3d2
潘国忠 <panguozhong@wondersgroup.com>
2019-01-03 13:07:55 +0800

    新增

---
+++ outapi/src/main/java/com/three/outapi/OutapiApplication.java
@@ -0,0 +1,14 @@
+package com.three.outapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutapiApplication.class, args);
+    }
+
+}
+
